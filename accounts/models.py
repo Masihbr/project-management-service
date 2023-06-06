9165d5e8cde1b2445bbadd1397b63efa3d423262
masihbr <masihbr@gmail.com>
2023-06-06 13:47:19 +0430

    Add User model with role field

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        DEVELOPER = 'developer', _('Developer')
+        PRODUCT_MANAGER = 'product_manager', _('Product Manager')
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        null=True,
+        default=None,
+        verbose_name=_('Role'),
+    )
