0cee910f4ea55e4b221c26adcfb164ccef4e8575
masihbr <masihbr@gmail.com>
2023-06-06 16:55:52 +0430

    Add UserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from accounts import models as account_models
 
-# Register your models here.
+
+@admin.register(account_models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "role", "is_active"]
+    list_filter = ["role"]
+    search_fields = ["username"]
