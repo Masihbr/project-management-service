8de3117d3a8473df6bf29a53fe70e0d89b1d2751
masihbr <masihbr@gmail.com>
2023-06-06 20:49:15 +0430

    Add admin models for projects app

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from projects import models as project_models
 
-# Register your models here.
+
+@admin.register(project_models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "created", "updated", "creator"]
+    search_fields = ["creator"]
+
+
+@admin.register(project_models.Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "status", "created", "updated", "creator"]
+    search_fields = ["creator"]
