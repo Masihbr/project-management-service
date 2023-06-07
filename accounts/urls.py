c0758c569eb99b337a53caa3d483f650e648969e
masihbr <masihbr@gmail.com>
2023-06-07 13:06:41 +0430

    Fix typo in sign-up path

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('signup/', views.UserSignUpAPIView.as_view(), name='sing-up'),
+    path('signup/', views.UserSignUpAPIView.as_view(), name='sign-up'),
     path('login/', views.LoginAPIView.as_view(), name='login'),
 ]
